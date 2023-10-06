5ff83b48c8d1bea7e0365d44393172f7772c69df
hesennov <hasanhasanov345@gmail.com>
2023-10-06 20:40:15 +0400

    listbox

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import Menu from "./headless/Menu";
+import Listbox from "./headless/Listbox";
 import "./index.css";
 const App = () => {
   return (
-    <div className="bg-slate-700">
-      <Menu />
+    <div className="flex justify-around">
+      <div>
+        <Listbox />
+      </div>
+      <div>
+        <Menu />
+      </div>
     </div>
   );
 };
